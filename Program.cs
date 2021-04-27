29844466d97dd977a65d00962662f3552b4d9701
EddyBautista <bautista.j.eddy@gmail.com>
2021-04-27 14:01:47 -0500

    Abbreviate

--- Program.cs
+++ Program.cs
@@ -112,8 +112,49 @@ namespace Edabit
         }
 
 
+        //The word i18n is a common abbreviation of internationalization in the developer community, 
+        //    used instead of typing the whole word and trying to spell it correctly.Similarly, a11y
+        //    is an abbreviation of accessibility.
+
+        //Write a function that takes a string and turns any and all "words" (see below) within that
+        //string of length 4 or greater into an abbreviation, following these rules:
+
+
+        //A "word" is a sequence of alphabetical characters.By this definition, any other character 
+        //like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two 
+        //words(eg. "elephant" and "ride").
+        //The abbreviated version of the word should have the first letter, then the number of removed 
+        //characters, then the last letter(eg. "elephant ride" => "e6t r2e").
+
+        public static string Abbreviate(string input)
+        {
+            List<string> abrList = new List<string>();
+            var abbreviateArray = input.Split(' ');
+            foreach(string x in abbreviateArray)
+            {
+                Console.WriteLine(x);
+                int lengthOfWord = x.Length;
+                 if(lengthOfWord >= 4)
+                {
+                    int intMid = lengthOfWord - 2;
+
+                    string abrWord = x[0] + intMid.ToString() + x[x.Length - 1];
+                   // Console.WriteLine(abrWord);
+                    abrList.Add(abrWord);
+
+                }
+              
+                abrList.Add(x);
+
+            }
+            string result = string.Join(' ', abrList);
+            Console.WriteLine(result);
+            return "";
+        }
+
         static void Main(string[] args)
         {
+            Abbreviate("my. dog, isn't feeling very well.");
             
         }
     }
